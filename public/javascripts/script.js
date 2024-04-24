d41e963a26bdb8421cfa82c294e4594024d5546e
GUNUPURU SURAJ <146669517+98493255@users.noreply.github.com>
2024-04-24 21:28:42 +0530

    Sirst Commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,72 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+
+  // for tablet smooth
+  tablet: { smooth: true },
+
+  // for mobile
+  smartphone: { smooth: true }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+});
+
+
+var h1 = document.querySelectorAll('#page2 #scroll h1');
+
+h1.forEach(e => {
+    var h1Text = e.textContent;
+    var clutter = "";
+    var splittedText = h1Text.split("");
+    splittedText.forEach(ele => {
+        clutter += `<span>${ele}</span>`;
+    });
+    
+    e.innerHTML = clutter;
+});
+
+var tl = gsap.timeline()
+
+tl.to('#page2 #scroll h1 span', {
+    color: '#3a86ff',
+    stagger: 1,
+    scrollTrigger: {
+        scroller: "#main",
+        trigger: "#page2 #scroll h1",
+        scrub: 4,
+        start: "top 100%",
+        end: "top 40%"
+    }
+});
+
+tl.from("#page2 img",{
+    scale:0,
+    opacity:0,
+    scrollTrigger: {
+        scroller: "#main",
+        trigger: "#page2  img",
+        scrub: 1,
+        // markers:true,
+        start: "top 100%",
+        end: "top 40%"
+    }
+})
+
+
