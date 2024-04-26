c3943834ab740af44e3905cb47db75101e6b532e
GUNUPURU SURAJ <146669517+98493255@users.noreply.github.com>
2024-04-26 07:41:15 +0530

    Did some changes in the Vote page and Upcoming Page!

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,17 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   res.render('index');
 });
+router.get('/vote', function(req, res, next) {
+  res.render('vote');
+});
+
+router.get('/map/:stateName/', function(req, res, next) {
+  var stateName = req.params.stateName;
+  res.render('map', { state: stateName });
+});
+router.get('/upcoming/:coming/', function(req, res, next) {
+  var coming = req.params.coming.toLocaleUpperCase().replace('-'," ");
+  res.render('upcoming', { coming: coming });
+});
 
 module.exports = router;
