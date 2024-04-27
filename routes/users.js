284c5898a8804be63ac0ebc8e6dc118f9f194750
GUNUPURU SURAJ <146669517+98493255@users.noreply.github.com>
2024-04-27 17:09:52 +0530

    Added the Details

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
+mongoose.connect('mongodb://127.0.0.1:27017/OVS');
 
-
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-
-
-module.exports = router;
+  const userSchema = new mongoose.Schema({
+  });
+ module.exports = mongoose.model('User', userSchema);
